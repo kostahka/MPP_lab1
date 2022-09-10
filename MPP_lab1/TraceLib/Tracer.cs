6596500a35bd5eb6f6b31b2d4cb35cc3983ecad0
Max <maks_kosten@mail.ru>
2022-09-10 21:55:14 +0300

    Start the project

---
+++ MPP_lab1/TraceLib/Tracer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TraceLib
+{
+    public interface ITracer
+    {
+        void StartTrace();
+        void StopTrace();
+        TraceResult GetTraceResult();
+    }
+    public class Tracer:ITracer
+    {
+        public void StartTrace()
+        {
+
+        }
+        public void StopTrace()
+        {
+
+        }
+        public TraceResult GetTraceResult()
+        {
+            return null;
+        }
+    }
+}
